3884fdda0596f3568a6fd4291ada683b1701171a
anshumanmahapatra <anshumanmahapatra214@gmail.com>
2022-04-02 11:28:06 +0530

    initial commit with firebase setup
    In the first step, we have setup our flutter app with Firebase.

---
+++ android/app/src/main/kotlin/com/example/aama_samaj_temp_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aama_samaj_temp_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import './views/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Home home = Get.put(const Home());
+    return GetMaterialApp(
+      home: home,
+    );
+  }
+}
